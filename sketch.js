56d9f996b8631b5c07e9a05be8a75b317e721a0b
José Lucas R. Santos <joselucasrs@gmail.com>
2022-03-15 03:54:13 -0300

    FInalizando vídeos.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ let grid = [];
 
 let current;
 
+let stack = [];
+
 function setup() {
   createCanvas(400, 400);
   cols = floor(width / w);
@@ -35,11 +37,16 @@ function draw() {
   if (next) {
     next.visited = true;
 
+    // step 2
+    stack.push(current);
+
     // step 3
     removeWalls(current, next);
 
     // step 4
     current = next;
+  } else if (stack.length > 0) {
+    current = stack.pop();
   }
 }
 
@@ -48,60 +55,6 @@ function index(i, j) {
     return -1;
   }
   return i + j * cols;
-}
-
-function Cell(i, j) {
-  this.i = i;
-  this.j = j;
-  this.walls = [true, true, true, true];
-  this.visited = false;
-
-  this.checkNeighbors = function () {
-    let neighbors = [];
-
-    let top = grid[index(i, j - 1)];
-    let right = grid[index(i + 1, j)];
-    let bottom = grid[index(i, j + 1)];
-    let left = grid[index(i - 1, j)];
-
-    if (top && !top.visited) neighbors.push(top);
-    if (right && !right.visited) neighbors.push(right);
-    if (bottom && !bottom.visited) neighbors.push(bottom);
-    if (left && !left.visited) neighbors.push(left);
-
-    if (neighbors.length > 0) {
-      let r = floor(random(0, neighbors.length));
-      return neighbors[r];
-    } else {
-      return undefined;
-    }
-  };
-
-  this.highlight = function () {
-    let x = this.i * w;
-    let y = this.j * w;
-
-    noStroke();
-    fill(0, 0, 255, 100);
-    rect(x, y, w, w);
-  };
-
-  this.show = function () {
-    let x = this.i * w;
-    let y = this.j * w;
-
-    stroke(255);
-    if (this.walls[0]) line(x, y, x + w, y);
-    if (this.walls[1]) line(x + w, y, x + w, y + w);
-    if (this.walls[2]) line(x + w, y + w, x, y + w);
-    if (this.walls[3]) line(x, y + w, x, y);
-
-    if (this.visited) {
-      noStroke();
-      fill(255, 0, 255, 100);
-      rect(x, y, w);
-    }
-  };
 }
 
 function removeWalls(a, b) {
